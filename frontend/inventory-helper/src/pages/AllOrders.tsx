fb32b79d5aa86a189874de2d5224f680e24dc9e4
Osban Cerejo <osbanacerejo@gmail.com>
2024-05-19 14:06:58 -0400

    Cleaned Home.tsx v1.0

--- frontend/inventory-helper/src/pages/AllOrders.tsx
+++ frontend/inventory-helper/src/pages/AllOrders.tsx
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function AllOrders() {
-  // const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Fetch orders when component mounts
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      // Make a request to your backend server
+      console.log("Succesful until here front end 1");
+      //   authenticate();
+      console.log("Succesful until here front end 2");
+      const response = await axios.get(
+        "http://localhost:3001/orders/allOrders"
+      );
+      setOrders(response.data.orders);
+    } catch (error) {
+      console.error("Fetch orders error:", error);
+    }
+  };
 
   return <div>AllOrders</div>;
 }
